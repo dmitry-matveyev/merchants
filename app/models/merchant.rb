e7b93f42c666bec1b59c59541bb68688debe48a0
dm <matveyev.dmitry@gmail.com>
2020-05-25 10:00:02 +0300

    move comment to the right place
    * minor fix

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
 class Merchant < ApplicationRecord
   enum status: { active: 1, inactive: 0 }
+
+  # As there can be many transactions we delete it by a single sql command
+  # assuming there are no callbacks needed
   has_many :transactions, inverse_of: :merchant, dependent: :delete_all
 end
